62c72585f836c84e9bde48e7de7b6ba796c01cb4
engshenoda <enhshenoda2020.com>
2025-08-15 19:16:16 +0300

    edit in splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+	  body: Center(
+		child: const Text('Splash Screen'),
+	  ),
+	);
   }
 }
