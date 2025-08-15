8412726d49a4117acb645e3a4c74c8bc79496f81
engshenoda <enhshenoda2020.com>
2025-08-15 19:17:00 +0300

    edit in login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: const Text('Login Screen'),
+      ),
+    );
   }
 }
